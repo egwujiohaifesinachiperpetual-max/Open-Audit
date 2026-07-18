a1768cfe388230914628c8aa537b4a7e0804f3fd
egwujiohaifesinachiperpetual-max <egwujiohaifesinachiperpetual@gmail.com>
2026-07-18 04:14:39 +0100

    Revert "feat(translator): add Soroswap Router blueprints"
    This reverts commit e4d5e4c58417f2e99761f6349ff056b947a3a7e8.

--- lib/translator/translations/es.ts
+++ lib/translator/translations/es.ts
@@ -12,29 +12,4 @@ export const ES_TRANSLATIONS = {
       Burn: "Quema",
     },
   },
-  soroswap: {
-    swap: (tokenIn: string, amountIn: string, tokenOut: string, amountOut: string) =>
-      `Intercambió ${amountIn} de [${tokenIn}] por ${amountOut} de [${tokenOut}]`,
-    addLiquidity: (
-      tokenA: string,
-      amountA: string,
-      tokenB: string,
-      amountB: string,
-      liquidity: string
-    ) =>
-      `Añadió ${amountA} de [${tokenA}] y ${amountB} de [${tokenB}] (${liquidity} tokens de liquidez)`,
-    removeLiquidity: (
-      tokenA: string,
-      amountA: string,
-      tokenB: string,
-      amountB: string,
-      liquidity: string
-    ) =>
-      `Retiró ${amountA} de [${tokenA}] y ${amountB} de [${tokenB}] al quemar ${liquidity} tokens de liquidez`,
-    eventTypes: {
-      Swap: "Intercambio",
-      AddLiquidity: "Añadir liquidez",
-      RemoveLiquidity: "Retirar liquidez",
-    },
-  },
 };
